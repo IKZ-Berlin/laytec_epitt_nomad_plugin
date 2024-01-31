441aedf709bdcfebb3afc1785b57493acad36252
Andrea Albino <andrea.albino@physik.hu-berlin.de>
2024-01-31 18:00:58 +0100

    improved plots

--- src/laytec_epitt/schema.py
+++ src/laytec_epitt/schema.py
@@ -75,16 +75,8 @@ class ReflectanceWavelengthTransient(PlotSection, ArchiveSection):
         shape=["*"],
         description="Normalized reflectance wavelength",
     )
-    total_line_number = Quantity(
-        type=np.dtype(np.int64),
-        description="""
-        The total line number found in the LayTec EpiTT data file.
-        This will help setting the correct autocorrelation starting point and autocorrelation period.
-        """,
-    )
     autocorrelation_starting_point = Quantity(
         type=np.int64,
-        default=0,
         description="""
         Add this parameter and save to smoothen the reflectance trace.
         Starting point of the window chosen for the autocorrelaation function calculation,
@@ -94,7 +86,6 @@ class ReflectanceWavelengthTransient(PlotSection, ArchiveSection):
     )
     autocorrelation_period = Quantity(
         type=np.int64,
-        default=4500,
         description="""
         Add this parameter and save to smoothen the reflectance trace.
         Period of the window chosen for the autocorrelaation function calculation,
@@ -241,13 +232,19 @@ class LayTecEpiTTMeasurement(InSituMeasurement, PlotSection, EntryData):
 
         # noise smoothening with autocorrelated function
         for trace in self.results[0].reflectance_wavelengths:
-            trace.total_line_number = len(trace.raw_intensity)
+            trace.autocorrelation_starting_point = 0
+            trace.autocorrelation_period = len(trace.raw_intensity)
             if (
                 trace.autocorrelation_period is not None
                 and trace.autocorrelation_starting_point is not None
             ):
                 trace.autocorrelated_intensity = sm.tsa.acf(
-                    trace.raw_intensity[trace.autocorrelation_starting_point :],
+                    trace.raw_intensity[
+                        trace.autocorrelation_starting_point : (
+                            trace.autocorrelation_starting_point
+                            + trace.autocorrelation_period
+                        )
+                    ],
                     nlags=trace.autocorrelation_period,
                     fft=False,
                 )
@@ -255,7 +252,13 @@ class LayTecEpiTTMeasurement(InSituMeasurement, PlotSection, EntryData):
         # plots
         if self.results[0]:
             overview_fig = make_subplots(
-                rows=2, cols=1, subplot_titles=["Reflectance", "Temperature"]
+                rows=3,
+                cols=1,
+                subplot_titles=[
+                    "Reflectance",
+                    "Autocorrelated Reflectance",
+                    "Temperature",
+                ],
             )
             temperature_figure = px.scatter(
                 x=self.results[0].process_time,
@@ -266,79 +269,106 @@ class LayTecEpiTTMeasurement(InSituMeasurement, PlotSection, EntryData):
             temperature_figure.update_traces(mode="markers", marker={"size": 2})
             temperature_figure.update_xaxes(title_text="Time [s]")
             temperature_figure.update_yaxes(title_text="Temperature [°C]")
-            overview_fig.add_trace(temperature_figure.data[0], row=2, col=1)
+            overview_fig.add_trace(temperature_figure.data[0], row=3, col=1)
             multi_reflec_fig = make_subplots(rows=1, cols=1)
-            for i, _ in enumerate(self.results[0].reflectance_wavelengths):
-                reflec_fig = go.Figure(
-                    # config={"displayModeBar": True, "scrollZoom": True}
+            multi_reflec_autocorr_fig = make_subplots(rows=2, cols=1)
+            for i, trace in enumerate(self.results[0].reflectance_wavelengths):
+                single_trace_fig = make_subplots(
+                    rows=2,
+                    cols=1,
+                    subplot_titles=["Reflectance", "Autocorrelated Reflectance"],
                 )
-                reflec_fig.add_trace(
+                single_trace_fig.add_trace(
                     go.Scatter(
-                        x=self.results[0].process_time.magnitude,
-                        y=self.results[0].reflectance_wavelengths[i].raw_intensity,
+                        x=self.results[0].process_time.magnitude[
+                            trace.autocorrelation_starting_point : (
+                                trace.autocorrelation_starting_point
+                                + trace.autocorrelation_period
+                            )
+                        ],
+                        y=trace.raw_intensity[
+                            trace.autocorrelation_starting_point : (
+                                trace.autocorrelation_starting_point
+                                + trace.autocorrelation_period
+                            )
+                        ],
                         mode="lines+markers",
                         line={"width": 2},
                         marker={"size": 2},
-                        name=f"{self.results[0].reflectance_wavelengths[i].wavelength.magnitude} nm",
-                    )
+                        name=f"{trace.wavelength.magnitude} nm",
+                    ),
+                    row=1,
+                    col=1,
                 )
                 multi_reflec_fig.add_trace(
                     go.Scatter(
                         x=self.results[0].process_time.magnitude,
-                        y=self.results[0].reflectance_wavelengths[i].raw_intensity,
+                        y=trace.raw_intensity,
                         mode="lines+markers",
                         line={"width": 2},
                         marker={"size": 2},
-                        name=f"{self.results[0].reflectance_wavelengths[i].wavelength.magnitude} nm",
+                        name=f"{trace.wavelength.magnitude} nm",
                     )
                 )
                 if (
-                    self.results[0].reflectance_wavelengths[i].autocorrelated_intensity
-                    is not None
-                    and self.results[0]
-                    .reflectance_wavelengths[i]
-                    .autocorrelated_intensity.any()
+                    trace.autocorrelated_intensity is not None
+                    and trace.autocorrelated_intensity.any()
                 ):
-                    reflec_fig.add_trace(
+                    single_trace_fig.add_trace(
                         go.Scatter(
-                            x=self.results[0].process_time.magnitude,
-                            y=self.results[0]
-                            .reflectance_wavelengths[i]
-                            .autocorrelated_intensity,
+                            x=self.results[0].process_time.magnitude[
+                                trace.autocorrelation_starting_point : (
+                                    trace.autocorrelation_starting_point
+                                    + trace.autocorrelation_period
+                                )
+                            ],
+                            y=trace.autocorrelated_intensity[
+                                trace.autocorrelation_starting_point : (
+                                    trace.autocorrelation_starting_point
+                                    + trace.autocorrelation_period
+                                )
+                            ],
                             mode="lines+markers",
                             line={"width": 2},
                             marker={"size": 2},
-                            name=f"Autocorr. {self.results[0].reflectance_wavelengths[i].wavelength.magnitude} nm",
-                        )
+                            name=f"Autocorr. {trace.wavelength.magnitude} nm",
+                        ),
+                        row=2,
+                        col=1,
                     )
-                    multi_reflec_fig.add_trace(
+                    multi_reflec_autocorr_fig.add_trace(
                         go.Scatter(
                             x=self.results[0].process_time.magnitude,
-                            y=self.results[0]
-                            .reflectance_wavelengths[i]
-                            .autocorrelated_intensity,
+                            y=trace.autocorrelated_intensity,
                             mode="lines+markers",
                             line={"width": 2},
                             marker={"size": 2},
-                            name=f"Autocorr. {self.results[0].reflectance_wavelengths[i].wavelength.magnitude} nm",
+                            name=f"Autocorr. {trace.wavelength.magnitude} nm",
                         )
                     )
-                reflec_fig.update_xaxes(title_text="Time [s]")
-                reflec_fig.update_yaxes(title_text="Reflectance")
-                reflec_fig.update_layout(
+                single_trace_fig.update_xaxes(title_text="Time [s]")
+                single_trace_fig.update_yaxes(title_text="Reflectance")
+                single_trace_fig.update_layout(
                     height=500,
                     # width=800,
                     showlegend=True,
                 )
-                self.results[0].reflectance_wavelengths[i].figures = [
+                single_trace_fig_json = single_trace_fig.to_plotly_json()
+                # single_trace_fig_json["config"] = {
+                #     "displayModeBar": True,
+                #     "scrollZoom": True,
+                # }
+                trace.figures = [
                     PlotlyFigure(
-                        label=f"{self.results[0].reflectance_wavelengths[i].wavelength.magnitude} nm",
+                        label=f"{trace.wavelength.magnitude} nm",
                         index=1,
-                        figure=reflec_fig.to_plotly_json(),
+                        figure=single_trace_fig_json,
                     )
                 ]
             for trace in multi_reflec_fig.data:
                 overview_fig.add_trace(trace, row=1, col=1)
+            for trace in multi_reflec_autocorr_fig.data:
+                overview_fig.add_trace(trace, row=2, col=1)
 
             overview_fig.update_layout(
                 # title_text="Reflectance",
@@ -348,7 +378,7 @@ class LayTecEpiTTMeasurement(InSituMeasurement, PlotSection, EntryData):
                 #     y=0.2,
                 #     bgcolor="rgba(255, 255, 255, 0)",  # Transparent background
                 #     bordercolor="rgba(255, 255, 255, 0)",  # Transparent border
-                height=2500,
+                height=4000,
                 # width=800,
                 showlegend=True,
             )
@@ -363,10 +393,11 @@ class LayTecEpiTTMeasurement(InSituMeasurement, PlotSection, EntryData):
             #     ax=0,
             #     ay=-40,
             # )
-
-            self.figures = [
-                PlotlyFigure(label="figure 1", figure=overview_fig.to_plotly_json())
-            ]
+            overview_fig_json = overview_fig.to_plotly_json()
+            for subplot in overview_fig_json["data"]:
+                subplot["config"] = {"displayModeBar": True, "scrollZoom": True}
+            # overview_fig_json["config"] = {"displayModeBar": True, "scrollZoom": True}
+            self.figures = [PlotlyFigure(label="figure 1", figure=overview_fig_json)]
 
 
 #            if self.process.reference:
